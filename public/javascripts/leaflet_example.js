b06ded94a93b524624ee142a791758aa0f7c8830
Joao Veloso <joao.veloso@auroraer.com>
2020-11-13 18:01:04 +0000

    java and pug map set up

--- public/javascripts/leaflet_example.js
+++ public/javascripts/leaflet_example.js
@@ -5,10 +5,30 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.5, -0.09]).addTo(map)
-// .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-// .openPopup();
+
+if (!navigator.geolocation) {
+    // Show location error
+    // locationErrorMessage = "Location not found. Try refreshing the page and allowing location permissions."
+    // handleRouteGenerationFormError(locationErrorMessage, true)
+
+    // Refresh the page
+    location.reload();
+
+} else {
+    // Hide location error
+    // handleRouteGenerationFormError("", false)
+    map.locate({ setView: true, maxZoom: 16 });
+}
+
+function onLocationFound(e) {
+    var currentLocationMarker = L.marker(e.latlng).addTo(map).bindPopup("You are here.").openPopup();
+    mapLayers.push(currentLocationMarker);
+    user_location = [e.latlng.lat, e.latlng.lng];
+    user_location_element.value = user_location;
+}
+
+map.on('locationfound', onLocationFound);
+
 
 $(window).on("resize", function () {
     $("#map").height($(window).height()).width($(window).width());
